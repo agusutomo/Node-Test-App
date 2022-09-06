f6e34d07ae8aa3068c0cd093f33770cc9b49cac8
Agus Utomo <agusutomo@kinsta.com>
2022-09-06 11:25:07 +0700

    Files Added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,6 +10,6 @@ app.get('/kinsta', (req, res) => {
   res.send('Hello Kinsta!')
 })
 
-app.listen(process.env.PORT, () => {
-  console.log("Example app listening on port ${process.env.PORT}")
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
