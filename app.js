d8a6cb28a70958418f22d55b2bca1d492b75a6f0
Agus Utomo <agusutomo@kinsta.com>
2022-09-06 11:27:31 +0700

    Files Added

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.get('/kinsta', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log("Example app listening on port ${port}")
 })
